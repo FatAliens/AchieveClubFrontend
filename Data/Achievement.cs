bb096c9da7e9847fd4530727c292f3066e5b3530
Vlad Fedorovich <72762763+sskef-web@users.noreply.github.com>
2024-09-13 00:57:02 +0300

    Update TopClubs & Add Achievement class

---
+++ Data/Achievement.cs
@@ -0,0 +1,12 @@
+﻿namespace AchieveClubFrontend.Data
+{
+    public class Achievement
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Xp { get; set; }
+        public string logoURL { get; set;}
+        public int completionRatio { get; set; }
+    }
+}
