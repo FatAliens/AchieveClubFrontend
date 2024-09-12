7b362ff747151ef586c45ff196741a5c68a6110f
Vlad Fedorovich <72762763+sskef-web@users.noreply.github.com>
2024-09-13 00:57:24 +0300

    Club page linked to api & updated

---
+++ Data/Club.cs
@@ -0,0 +1,13 @@
+﻿using AchieveClubFrontend.Data;
+
+public class Club
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Address { get; set; }
+    public string LogoURL { get; set; }
+    public int AvgXp { get; set; }
+    public List<User> Users { get; set; }
+    public int Position { get; set; }
+}
