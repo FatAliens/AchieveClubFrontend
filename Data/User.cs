0cd665769e4cb75936c576e9c48930ffc4b198c7
Vlad Fedorovich <72762763+sskef-web@users.noreply.github.com>
2024-09-13 00:55:11 +0300

    TopUsers page linked to api & updated

---
+++ Data/User.cs
@@ -0,0 +1,15 @@
+﻿namespace AchieveClubFrontend.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Avatar { get; set; }
+        public int ClubId { get; set; }
+        public string ClubName { get; set; }
+        public string ClubLogo { get; set; }
+        public int XpSum { get; set; }
+        public int Position { get; set; }
+    }
+}
